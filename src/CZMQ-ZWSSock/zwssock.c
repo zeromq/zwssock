ea199e9f8e15c0dec045da4208cf50f57c10e5e7
Stefan de Konink <stefan@konink.de>
2017-04-30 00:51:14 +0200

    Remove the parsing of more from the zwsdecoder.c
    Currently the more information is parsed in the zwsdecoder. This means our decoder would only work in case we have "normal" ZeroMQ frames. This code is the first step to give zwssock support for permessage-deflate as part of https://tools.ietf.org/html/draft-ietf-hybi-permessage-compression-28
    
    I would strongly urge not to blindly merge this commit, until properly reviewed. I haved used valgrind on the code, and did several tests with browser interaction. The step by step commits are compiled from the big permessage-deflate implementation to show the steps towards it.

--- src/CZMQ-ZWSSock/zwssock.c
+++ src/CZMQ-ZWSSock/zwssock.c
@@ -178,7 +178,7 @@ client_destroy(client_t **self_p)
 	}
 }
 
-void router_message_received(void *tag, byte* payload, int length, bool more)
+void router_message_received(void *tag, byte* payload, int length)
 {
 	client_t *self = (client_t *)tag;
 
@@ -188,7 +188,8 @@ void router_message_received(void *tag, byte* payload, int length, bool more)
 		zmsg_addstr(self->outgoing_msg, self->hashkey);
 	}
 
-	zmsg_addmem(self->outgoing_msg, payload, length);
+	bool more = (payload[0] == 1);
+	zmsg_addmem(self->outgoing_msg, &payload[1], length - 1);
 
 	if (!more)
 	{
